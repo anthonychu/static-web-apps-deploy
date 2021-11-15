94bd2dc06443d5f53f50c39dc5e7bff07a8fee6b
Anthony Chu <anthony@anthonychu.ca>
2021-11-14 22:59:27 -0800

    Fix directories

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -l
 
 # Build Next.js
+export ORIG_WORKING_DIR=${PWD}
 cd /nextjs2swa
 npm install
-node index.js
+cd ${ORIG_WORKING_DIR}
+node /nextjs2swa/index.js
 
 cd /bin/staticsites/
 ./StaticSitesClient $INPUT_ACTION
