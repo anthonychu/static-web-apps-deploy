558b0b18e46160d51eafffbcc34372a66242af02
Anthony Chu <anthony@anthonychu.ca>
2021-11-15 23:53:17 -0800

    Custom handler redirect workaround

--- nextjs2swa/functions_files/server.js
+++ nextjs2swa/functions_files/server.js
@@ -17,6 +17,25 @@ async function main() {
             req.url = parsedUrl.pathname + (parsedUrl.search || "")
         }
         res.setHeader('x-ms-nextjs-render', 'server')
+
+        // HACK: To get around custom handler bug that doesn't return redirects
+        // Use JavaScript to redirect to the correct URL
+        // https://github.com/Azure/azure-functions-host/issues/7860
+        const origWriteHead = res.writeHead
+        res.writeHead = function (statusCode) {
+            if ([301, 302, 303, 307].includes(statusCode)) {
+                origWriteHead.apply(res, [200])
+                res.end(`
+                    <script>
+                        window.history.replaceState(null, null, "${res.getHeader('location')}")
+                        window.location.reload()
+                    </script>
+                `)
+            } else {
+                origWriteHead.apply(res, arguments)
+            }
+        }
+
         handle(req, res, parsedUrl)
     }).listen(port, (err) => {
         if (err) throw err
